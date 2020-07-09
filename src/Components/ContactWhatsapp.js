af72316690852d5b17a5ef29abc47166a155cf12
Anurag Gharat <53979540+anurag-gharat@users.noreply.github.com>
2020-07-09 15:17:19 +0530

    footer changed

--- src/Components/ContactWhatsapp.js
+++ src/Components/ContactWhatsapp.js
@@ -5,16 +5,15 @@ export default function ContactWhatsapp() {
     return (
         <div className="parallax-box">
           <div className="container center white-text " >
-            <h3 className="bold-text">Contact Us!</h3>
+            <h3 className="bold-text">Contact us</h3>
           </div>
             <div className="container">
               <div className="row center">
                 <div className="col l4 s12">
                   <div className="card-panel">
                     <i className="material-icons teal-text display-2">sms</i>
-                    <h6>Whatsapp us at:</h6>
-                    <p>7745050822</p>
-                    <a className="btn btn2" target="_blank" rel="noopener noreferrer"  href="https://api.whatsapp.com/send?phone=8380032300">Whatsapp us? 
+                    <h6>Whatsapp us:</h6>
+                    <a className="btn btn2" target="_blank" rel="noopener noreferrer"  href="https://api.whatsapp.com/send?phone=8380032300">Whatsapp 
                     <i className="material-icons left">sms</i>
                     </a>
                   </div>
@@ -22,8 +21,7 @@ export default function ContactWhatsapp() {
                 <div className=" col l4 s12">
                 <div className="card-panel">
                     <i className="material-icons teal-text display-2">drafts</i>
-                    <h6>Mail us at:</h6>
-                    <p>Email</p>
+                    <h6>Mail us:</h6>
                     <a className="btn" target="_blank" href="mailto:littleearthfarmsandnursery@gmail.com"   rel="noopener noreferrer">Mail Now
                     <i className="material-icons left">drafts</i>
                     </a>
@@ -32,9 +30,8 @@ export default function ContactWhatsapp() {
                 <div className=" col l4 s12">
                 <div className="card-panel">
                     <i className="material-icons teal-text display-2">location_on</i>
-                    <h6>Find us at:</h6>
-                    <p>Little Earth , Varsoli Alibag</p>
-                    <a className="btn" href="#">Find in Map<i className="material-icons left">location_on</i>
+                    <h6>Find us:</h6>
+                    <a className="btn" target="_blank" href="https://goo.gl/maps/aYpdtLWWL6s5LNmX6">Find in Map<i className="material-icons left">location_on</i>
 
                     </a>
                   </div>
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,25 +1,33 @@
 import React from 'react'
 import {FaInstagram , FaFacebookF, FaGoogle, FaWhatsapp} from 'react-icons/fa'
+import logo from '../images/final.png'
 
 export default function Footer() {
     return (
-        <footer className="grey darken-4  row m-0 pt-4">
+        <footer className="grey lighten-5 row m-0 pt-4">
                 <div className="row m-0">
                     <div className="row ">
-                        {/* <h5 className="white-text center">Get in touch with us!</h5> */}
-                        <div className="flex d-row white-text">
-                                <a>
-                                <FaFacebookF className="social-media green-text"/>
+                        <div className="row m-0 valign-wrapper">
+                            <div className="col l6 s12">
+                                <img src={logo} alt="footer logo" width="100" />
+                                <h5 className="m-0 name green-text">Little Earth Farms & Nursery</h5>
+                                <p className="m-0">Knot With Nature</p>
+                                <p className="grey-text darken-4">Little Earth Farms and Nursery, Ahead Of Harmony Resort , Opp Prakash Patil Home , Parur , Karle Khind Alibag</p>
+                            </div>
+                            <div className="col l6 s12  ">
+                                <a className="right" href="" target="_blank">
+                                <FaInstagram  className="social-media"/>
                                 </a>
-                                <a>
-                                <FaInstagram  className="social-media green-text"/>
+                                <a className="right" href="https://api.whatsapp.com/send?phone=8380032300" target="_blank">
+                                <FaWhatsapp  className="social-media"/>
                                 </a>
-                                <a>
-                                <FaWhatsapp  className="social-media green-text"/>
+                                <a className="right" href="mailto:littleearthfarmsandnursery@gmail.com" target="_blank">
+                                <FaGoogle className="social-media"/>
                                 </a>
-                                <a>
-                                <FaGoogle className="social-media green-text"/>
+                                <a className="right" href="https://www.facebook.com/Little-Earth-Farms-and-Nursery-421292694868761/?hc_ref=PAGES_TIMELINE" target="_blank">
+                                <FaFacebookF className="social-media"/>
                                 </a>
+                                </div>
                         </div>
                     </div>
                     <div className="row m-0 black py-3">
