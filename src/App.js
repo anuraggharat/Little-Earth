7a21ab540b177dc09bf615b7c4980682daee1fc2
Anurag Gharat <53979540+anurag-gharat@users.noreply.github.com>
2021-02-03 20:26:14 +0530

    seo changed in little earth

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React, { useEffect } from 'react';
-import './App.css';
-import Hero from './Components/Hero';
-import Navbar from './Components/Navbar';
-import About from './Components/About';
-import Footer from './Components/Footer';
-import Services from './Components/Services'
-import ContactWhatsapp from './Components/ContactWhatsapp';
-import Features from './Components/Features';
-import Testimonials from './Components/Testimonials';
-import ItemSlider from './Components/ItemSlider';
-import M from  'materialize-css/dist/js/materialize.min.js';
-import Work from './Components/Work';
-
-
+import React, { useEffect } from "react";
+import "./App.css";
+import Hero from "./Components/Hero";
+import Navbar from "./Components/Navbar";
+import About from "./Components/About";
+import Footer from "./Components/Footer";
+import Services from "./Components/Services";
+import ContactWhatsapp from "./Components/ContactWhatsapp";
+import Features from "./Components/Features";
+import Testimonials from "./Components/Testimonials";
+import ItemSlider from "./Components/ItemSlider";
+import M from "materialize-css/dist/js/materialize.min.js";
+import Work from "./Components/Work";
 
 function App() {
-
-  useEffect(()=>{
+  useEffect(() => {
     M.AutoInit();
-  },[])
+  }, []);
 
   return (
     <div className="App">
